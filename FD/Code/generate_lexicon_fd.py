f5d4f44e5a1d5a12079ccc00f3e9641e227e2d77
daniela-wiepert <58085556+daniela-wiepert@users.noreply.github.com>
2021-05-09 22:11:56 -0400

    NEW FD MODEL

---
+++ FD/Code/generate_lexicon_fd.py
@@ -0,0 +1,231 @@
+from collections import Counter, defaultdict
+import pandas as pd
+#import ngram_modelfd
+import csv
+import os
+import json
+import numpy as np
+'''
+This file should calculate a set of all possible artificial words,
+up to an arbitrary length, based on the n-gram model probabilities. 
+Each artificial word should also be assigned its probability score. 
+'''
+
+# get phoneme features from PHOIBLE
+# note the path is resolved-phoible.csv that is corrected for mismatches between phonemes in PHOIBLE and the XPF Corpus
+phoneme_features = pd.read_csv("Data/resolved-phoible.csv")
+phoneme_features.drop(["InventoryID", "Glottocode","ISO6393","LanguageName","SpecificDialect","GlyphID","Allophones","Marginal","Source"], axis="columns", inplace=True)
+phoneme_features = phoneme_features.rename(columns={'periodicGlottalSource':'voice'})
+
+# list of all feature names in PHOIBLE table
+features = phoneme_features.copy()
+features.drop(["Phoneme","voice"],axis="columns", inplace=True)
+features = features.columns.values.tolist()
+
+# global variables
+word_dict = {} #dictionary of generated word: probability of word
+to_feat = {} #dictonary of phoneme: feature representation 
+phon_model = {} #dictionary of feature representation: {possible phonemes: # of occurrences}
+
+def change_to_feat(phoneme):
+    '''
+    '''
+    global to_feat
+    if to_feat.get(phoneme) is None:
+        return phoneme
+        
+    return to_feat.get(phoneme)
+
+
+def reconstruct_phon(feat):
+    '''
+    takes a feature and returns list of possible phonemes + their probabilities
+    '''
+    global phon_model
+    poss_dict = phon_model.get(feat)
+
+    phon = list(poss_dict.keys())
+    prob = list(poss_dict.values()) 
+    
+    return phon, prob
+
+#TODO: check this
+def reconstruct_con(context):
+    '''
+    takes a context with an imperfect representation and reconstructs it
+    '''
+    context = context.split(" ")
+    phon = []
+    prob = []
+    for feat in context:
+        p,pr = reconstruct_phon(feat)
+        phon.append(p)
+        pr = np.log(pr)
+        for i in range(len(pr)):
+            if pr[i] == float('-inf'):
+                pr[i] = 0.0
+        prob.append(pr)
+
+    contexts, context_prob = make_contexts(phon,prob)
+    #if len(contexts) > 1:
+       # print('hi')
+    return contexts,context_prob
+
+#TODO: check this
+def make_contexts(poss,prob,contexts=[],context_prob=[]):
+    '''
+    all possible contexts
+    '''
+    con = []
+    con_prob = []
+    if poss == []:
+        return contexts, context_prob
+    elif contexts == []:
+        p = poss[0]
+        pr = prob[0]
+        for i in range(len(p)):
+            con.append(p[i])
+            con_prob.append(pr[i])
+    else:
+        for i in range(len(contexts)):
+            c = contexts[i]
+            cpr = context_prob[i]
+            p = poss[0]
+            pr = prob[0]
+            for j in range(len(p)):
+                con.append(c + ' ' + p[j])
+                con_prob.append(pr[j]+cpr)
+    poss.pop(0)
+    prob.pop(0)
+    return make_contexts(poss,prob,con,con_prob)
+
+def generate_phoneme(model, wordlen=8, context='', n=4, current_prob=None):
+    context = context.split(" ")
+    n_context = context[-n:]
+    seg_counts = model.get(" ".join(n_context)) # dictionary of new segments 
+    total_count = sum(seg_counts.values())
+    in_seg = {}
+    for seg in seg_counts.keys(): # going through all possible predicted consonants
+        feat = change_to_feat(seg)
+        if in_seg.get(feat) is None:
+            in_seg[feat] = 0
+        in_seg[feat] += seg_counts[seg]
+    
+    for seg in in_seg.keys():
+        new_context = " ".join(context) + " " + seg
+        prob = in_seg[seg] / total_count
+        prob = np.log(prob)
+        if prob == float('-inf'):
+            prob = 0.0
+        # continue generating the rest of this word
+        generate_lexicon(model, wordlen, new_context, n, current_prob + prob)
+    
+def initial_context_prob(model, initial_context):
+    '''
+    get initial probabilites
+    input: model  and initial context
+    outpu: initial probability
+    '''
+    freq = sum(model[initial_context].values())
+    total_count = 0
+    for k, v in model.items():
+        if k[:3] == "[_w":
+            total_count += sum(v.values())
+    prob = freq / total_count
+    return np.log(prob)
+
+def generate_lexicon(model, wordlen=8, context="", n=4, current_prob=None):
+    '''
+    Generate all possible words for this set of models. 
+
+    Params: 
+     - cv_model: consonant-vowel model dictionary (see ngram_model.py)
+     - c_model: consonant model dictionary (see ngram_model.py)
+     - v_model: vowel model dictionary (see ngram_model.py)
+     - wordlen: cutoff length for generated words (including [_w and ]_w)
+     - context: string representation of entire word up to this point 
+                (not the previous n segs, but the entire word)
+     - n: length of model's prior context
+     - current_prob: the probability of generating the word (context) up till this point
+
+    Returns:
+     - Nothing, modifies global dictionary word_dict
+    '''
+    global word_dict
+
+    if context == "": # there is no prior context, starting a new word
+        for prev_context in model.keys():
+            if prev_context[0:3] == '[_w':
+                if model.get(prev_context) != None:
+                    prob = initial_context_prob(model, prev_context)
+                    func_context = prev_context
+                    if prob == float('-inf'):
+                            prob = 0.0
+                    generate_phoneme(model, wordlen,func_context,n,prob)
+    else:
+        if context[-3:] == "]_w":
+            poss_contexts, poss_probs = reconstruct_con(context[4:-4])
+            for i in range(len(poss_contexts)):
+                word_dict['[_w ' + poss_contexts[i] + ' ]_w'] = current_prob + poss_probs[i]
+
+        else:
+            context = context.split(" ")
+            if len(context) < wordlen:
+                n_context = context[-n:] # getting only previous n segments
+                n_context = " ".join(n_context)
+                context = " ".join(context)
+
+                if model.get(n_context) != None:
+                    generate_phoneme(model, wordlen, context, n, current_prob)
+                                
+
+def main():
+    global word_dict 
+    global to_feat
+    global phon_model
+
+    lang_codes = []
+    identity = '5000_3'
+    #identity = 'practice'
+
+    with open("Data/lang_codes"+identity+".tsv", 'r') as fin:
+        reader = csv.reader(fin, delimiter='\t')
+        lang_codes = list(reader)
+
+    for lang_code in lang_codes:
+        lang_code = ''.join(lang_code)
+        print("lang code: ", lang_code)
+
+        infile = './Data/utf8_ngram_models/'
+        
+        with open(infile + lang_code + "_model.json", 'r', encoding='utf8') as fin:
+            model = json.load(fin)
+
+        with open(infile + lang_code + "_phon_model.json", 'r', encoding='utf8') as fin:
+            phon_model = json.load(fin)
+        
+        with open(infile + lang_code + "_to_feat.json", 'r', encoding='utf8') as fin:
+            to_feat = json.load(fin)
+
+        generate_lexicon(model)
+
+        #exponentiate everything in the word dict
+        word_dict = {key: np.exp(value) for key, value in word_dict.items()}
+
+        outfile  = './Data/generated_words/'
+        if not os.path.exists(outfile):
+            os.mkdir(outfile)
+        with open(outfile + lang_code + "_generated_words.json", 'w', encoding='utf8') as fout:
+            json.dump(word_dict, fout, ensure_ascii=False)
+
+        # resetting the dictionariesfor the next languages
+        word_dict = {}
+        to_feat = {}
+        phon_model = {}
+
+
+    return None
+
+
+if __name__ == "__main__":
+    main()
---
+++ FD/FD-README.md
@@ -0,0 +1,39 @@
+# XPF-soft-constraints
+# FINAL-OBSTRUENT DEVOICING 
+UP TO DATE AS OF: 05/09/2021
+List of project files/dependencies
+
+** NOTE: for any .py script, you may need to change internal file name variables in order to access XPF directory files
+
+1. resolve_phoible.R
+      * this does not need to be run, but if there are any phonemes that are missing but have a close match, you can add the phoneme pair to this file to change the phoible phoneme to the one that matches the transcription. Phoible has some weird extra characters on the phonemes that can make matching impossible.
+
+2. all_counts04.py
+    * gets phonemic translations for words in all languages in the laguage list
+    * can specify: language list (default: Code/langs-list-03.tsv)
+                   arguments for stopatn - # types (default: 5000), cut-off frequency (default: 3)
+    * file dependencies:
+          access to XPF directory
+          sumstats01.py
+          translate04.py
+          stopatn.sh
+    * output: a tsv file containing language code and translated words (word_list.tsv)
+
+3.  ngram_model_fd.py
+    * originally written by Rebecca Mathew
+    * modified by Daniela Wiepert
+    * creates an ngram model (default n = 4)
+    * file dependencies:
+        word_list*.tsv (generated from all_counts04.py)
+        resolved-phoible.csv (generated from resolve_phoible.R)
+    * output: model, phon_model, to_feat    
+        - model is a dictionary with the n-gram context (ie. '[_w a m i') as a key, and the value is another dictionary containing the phonemes that follow and the number of times they follow a given context. Changes any occurrences of obstruents appearing word-finally into a feature string containing all the features from phoible EXCEPT voicing information - so /k/ might change to 'consonant/SegmentClass|-/sonorant|-/continuant|....', whereas vowels and non-sonorants have perfect representations - /a/ = 'a', /m/ = 'm',  etc. Removing voicing information allows the model to generate lexicons with final voicing contrasts that occur with equal frequency. This current model does it any time it sees an obstruent, but this shouldn't cause an issue since if given a context with 'k a g', the model will generate a lexicon with 'g a k' 'g a g' 'k a k' 'k a g', so with many samples, it should balance? QUESTION THIS? An improvement may be to do this only for final contexts 'g a OBSTRUENT ]_w'
+        - phon_model is a dictionary mapping a feature representation to another dictionary containing the phonemes that the feature corresponds with plus the number of occurrences of each phoneme
+        - to_feat is a dictionary mapping phonemes to their corresponding feature representation
+    
+4. generate_lexicon_fd.py
+    * originallywritten by Rebecca Mathew
+    * modified by Daniela Wiepert
+    * This code generates all possible words that can be created given contexts + phoneme representations + probabilites.
+    * all combined probabilites are calculated as log probabilites
+
